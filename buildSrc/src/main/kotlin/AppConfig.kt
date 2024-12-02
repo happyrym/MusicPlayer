0c86bd077a925fe8842d3382cbb71d4f692e746c
rymin <rymin@clllin.com>
2024-12-02 23:46:06 +0900

    feat: kts 정리

---
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -0,0 +1,11 @@
+object AppConfig {
+    const val targetSdk = 35
+    const val minSdk = 26
+
+    object MUSICPLAYER {
+        const val versionCode = 100001 // VERSION 001 QA 00 BUILD 001
+        const val fixedVersionName = "1.0.0"
+        const val debugVersionNameSuffix = ".0.1"
+        const val applicationId = "com.rymin.musicplayer"
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "MusicPlayer"
 include(":app")
- 
